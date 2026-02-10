a15ed70d6be515b49d6001f0fbd6c8e91ea705c3
katttiger <katttiger@gmail.com>
2026-02-10 11:05:14 +0100

    Initial commit

---
+++ src/main/java/se/iths/cecilia/postrgreszoo/PostrgresZooApplication.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.postrgreszoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostrgresZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostrgresZooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/cecilia/postrgreszoo/PostrgresZooApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.postrgreszoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostrgresZooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
